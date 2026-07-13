1483230375ab6fd67c88702d2c8a2213c1715ea7
bliss-tech-solutions <developer.bliss@gmail.com>
2026-07-13 12:05:29 +0530

    remove unwanted link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,6 @@ const properties = [
   { label: "Shree Mangaldeep", href: "/projects/shree-mangaldeep-apartments" },
   { label: "Sneh Greens", href: "/projects/sneh-greens" },
   { label: "Sneh Residency", href: "/projects/sneh-residency" },
-  { label: "Mangal Deep", href: "/projects/mangaldeep-apartment" },
 ];
 
 const contactInfo = [
